35897d1c426eb20fffd399ff757eb9ebd69406d8
JaySebagh <jaysebagh@gmail.com>
2019-01-22 13:51:51 -0800

    finished array methods mvp questions

--- assignments/array-methods.js
+++ assignments/array-methods.js
@@ -54,30 +54,93 @@ const runners = [{"id":1,"first_name":"Charmain","last_name":"Seiler","email":"c
 {"id":50,"first_name":"Shell","last_name":"Baine","email":"sbaine1d@intel.com","shirt_size":"M","company_name":"Gabtype","donation":171}];
 
 // ==== Challenge 1: Use .forEach() ====
-// The event director needs both the first and last names of each runner for their running bibs.  Combine both the first and last names into a new array called fullName. 
+// The event director needs both the first and last names of each runner for their running
+// bibs.  Combine both the first and last names into a new array called fullName. 
 let fullName = [];
+
+runners.forEach(function(newNames) {
+  let bothNames = `${newNames.first_name} ${newNames.last_name}`;
+  fullName.push(bothNames)
+});
+
 console.log(fullName);
 
 // ==== Challenge 2: Use .map() ====
-// The event director needs to have all the runner's first names converted to uppercase because the director BECAME DRUNK WITH POWER. Convert each first name into all caps and log the result
+// The event director needs to have all the runner's first names converted to uppercase
+// because the director BECAME DRUNK WITH POWER. Convert each first name into all caps
+// and log the result
 let allCaps = [];
-console.log(allCaps); 
+
+allCaps = runners.map(function(newNames) {
+  let firstNames = newNames.first_name.toUpperCase();
+  return firstNames
+});
+
+console.log(allCaps);
 
 // ==== Challenge 3: Use .filter() ====
-// The large shirts won't be available for the event due to an ordering issue.  Get a list of runners with large sized shirts so they can choose a different size. Return an array named largeShirts that contains information about the runners that have a shirt size of L and log the result
+// The large shirts won't be available for the event due to an ordering issue.  
+// Get a list of runners with large sized shirts so they can choose a different size.
+// Return an array named largeShirts that contains information about the runners that
+// have a shirt size of L and log the result
 let largeShirts = [];
+
+largeShirts = runners.filter(big => big.shirt_size === "L");
+
 console.log(largeShirts);
 
 // ==== Challenge 4: Use .reduce() ====
-// The donations need to be tallied up and reported for tax purposes. Add up all the donations into a ticketPriceTotal array and log the result
+// The donations need to be tallied up and reported for tax purposes.
+// Add up all the donations into a ticketPriceTotal array and log the result
 let ticketPriceTotal = [];
-console.log(ticketPriceTotal);
+
+runners.forEach(function(newDono) {
+  let theDono = newDono.donation;
+  ticketPriceTotal.push(theDono)
+});
+
+ticketPriceTotal = ticketPriceTotal.reduce(function(total, current) {
+  return total + current;
+})
+
+console.log(ticketPriceTotal)
 
 // ==== Challenge 5: Be Creative ====
-// Now that you have used .forEach(), .map(), .filter(), and .reduce().  I want you to think of potential problems you could solve given the data set and the 5k fun run theme.  Try to create and then solve 3 unique problems using one or many of the array methods listed above.
+// Now that you have used .forEach(), .map(), .filter(), and .reduce().
+// I want you to think of potential problems you could solve given the data set and
+//the 5k fun run theme.  Try to create and then solve 3 unique problems using one or
+//many of the array methods listed above.
 
 // Problem 1
 
+// We need to know how many people are size S
+let smallShirts = [];
+
+smallShirts = runners.filter(small => small.shirt_size === "S");
+
+console.log(smallShirts);
+
 // Problem 2
 
+// We need to message everyone that their item has been shipped. Get all the emails.
+
+let emailList = [];
+
+runners.forEach(function(allEmails) {
+  let listEmails = allEmails.email;
+  emailList.push(listEmails)
+});
+
+console.log(emailList);
+
+
 // Problem 3
+
+// We want to send a special thanks to anyone who donated over 200.
+//  Get a list including people who donated 200+
+
+let donoTwoHundred = [];
+
+donoTwoHundred = runners.filter(dono => dono.donation >= 200);
+
+console.log(donoTwoHundred);
